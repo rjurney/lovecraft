6f800392890b7ad6e50e5686fad4b3ee2ffa500c
Russell Jurney <russell.jurney@gmail.com>
2024-03-06 21:04:33 -0800

    Replaced old script with new one that works

--- lovecraft/crawl.py
+++ lovecraft/crawl.py
@@ -1,30 +1,34 @@
+import os
+
 import scrapy
 from bs4 import BeautifulSoup
 
+# Don't flood the server with requests
+os.environ["DOWNLOAD_DELAY"] = 0.25
+
 
 class LovecraftSpider(scrapy.Spider):
+    """LovecraftSpider Retrieves Lovecraft stories from hplovecraft.com
+
+    This spider retrieves the URLs of Lovecraft stories from the hplovecraft.com website
+    """
+
     name = "lovecraft"
     allowed_domains = ["hplovecraft.com"]
     start_urls = ["https://www.hplovecraft.com/writings/texts/"]
-    custom_settings = {"FEED_FORMAT": "jsonlines", "FEED_URI": "lovecraft_stories.jl"}
+    custom_settings = {"FEED_FORMAT": "jsonlines", "FEED_URI": "data/stories.json"}
 
     def parse(self, response):
         # Extract and follow story links that match the expected pattern
-        soup = BeautifulSoup(response.text)
-        url = "https://www.hplovecraft.com/writings/texts/"
-        prefix = "fiction/"
-        links = [
-            url + a["href"] for a in soup.find_all("a", href=True) if a["href"].startswith(prefix)
-        ]
-
-        # Print or return the links
+        links = response.css("a::attr(href)").re(r"^fiction/.+\.aspx$")
         for link in links:
-            print(link)
+            full_url = response.urljoin(link)
+            yield scrapy.Request(full_url, callback=self.parse_story)
 
     def parse_story(self, response):
         # Use BeautifulSoup to parse and extract the plaintext
-        soup = BeautifulSoup(response.text, "html.parser")
+        soup = BeautifulSoup(response.text, "lxml")
         # Assuming the story text might not always be within a specific id, we extract all text
         # You may need to adjust the extraction method based on the page structure
-        story_text = "\n".join([p.get_text(strip=True) for p in soup.select("p")])
-        yield {"url": response.url, "text": story_text}
+        # story_text = "\n".join([p.get_text(strip=True) for p in soup.select("p")])
+        yield {"url": response.url, "text": soup.get_text(strip=True)}
